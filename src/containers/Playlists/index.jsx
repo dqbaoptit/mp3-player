1b637bf04f715de38f5080598563a2e72c2914b9
Bao <dqbaoptit@gmail.com>
2020-04-25 09:56:58 +0700

    core

---
+++ src/containers/Playlists/index.jsx
@@ -0,0 +1,85 @@
+import AiDoiMinhDuocMai from '../../statics/mp3/Ai-Doi-Minh-Duoc-Mai-Thanh-Hung.mp3';
+import ChiKhiAnhMotMinh from '../../statics/mp3/Chi-Khi-Anh-Mot-Minh-Vu-Duy-Khanh-Dat-G.mp3';
+import DungLoAnhDoiMa from '../../statics/mp3/Dung-Lo-Anh-Doi-Ma-Mr-Siro.mp3';
+import EmCoBiet from '../../statics/mp3/Em-Co-Biet-H2K.mp3';
+import HonCaYeu from '../../statics/mp3/Hon-Ca-Yeu-Duc-Phuc.mp3';
+import LaDuyenKhongPhan from '../../statics/mp3/La-Duyen-Khong-Phan-Chung-Thanh-Duy.mp3';
+import SoPhaiKetThuc from '../../statics/mp3/So-Phai-Ket-Thuc-Nhat-Phong.mp3';
+import ThichThiDen from '../../statics/mp3/Thich-Thi-Den-Le-Bao-Binh.mp3';
+import CoPhuongTuThuong from '../../statics/mp3/Tinh-Sau-Thien-Thu-Muon-Loi-Doan-Hieu.mp3';
+import TrucXinh from '../../statics/mp3/Truc-Xinh-Minh-Vuong-M4U-Viet-ACV.mp3';
+
+import tbnail1 from '../../statics/img/aidoiminh.jpg'
+import tbnail2 from '../../statics/img/ckamm.jpg'
+import tbnail3 from '../../statics/img/dunglo.jpg'
+import tbnail4 from '../../statics/img/emcobiet.jpg'
+import tbnail5 from '../../statics/img/honcayeu.jpg'
+import tbnail6 from '../../statics/img/chungthanhduy.jpg'
+import tbnail7 from '../../statics/img/spketthuc.jpg'
+import tbnail8 from '../../statics/img/thithiden.jpg'
+import tbnail9 from '../../statics/img/cptt.jpg'
+import tbnail10 from '../../statics/img/trucxinh.jpg'
+
+
+export const playlist = [
+    {
+        src: AiDoiMinhDuocMai,
+        img: tbnail1,
+        title: "Ai đợi mình được mãi",
+        artist: "Thanh Hưng",
+    },
+    {
+        src: ChiKhiAnhMotMinh,
+        img: tbnail2,
+        title: "Chỉ khi anh một mình",
+        artist: "Vũ Duy Khánh - Đạt G",
+    },
+    {
+        src: DungLoAnhDoiMa,
+        img: tbnail3,
+        title: "Đừng lo, anh đợi mà",
+        artist: "Mr.Siro",
+    },
+    {
+        src: EmCoBiet,
+        title: "Em có biết ?",
+        artist: "H2K",
+        img: tbnail4,
+    },
+    {
+        src: HonCaYeu,
+        title: "Hơn cả yêu",
+        artist: "Đức Phúc",
+        img: tbnail5
+    },
+    {
+        src: LaDuyenKhongPhan,
+        title: "Là duyên không phận",
+        artist: "Chung Thanh Duy",
+        img: tbnail6
+    },
+    {
+        src: SoPhaiKetThuc,
+        title: "Sợ phải kết thúc",
+        artist: "Nhật Phong",
+        img: tbnail7
+    },
+    {
+        src: ThichThiDen,
+        title: "Thích thì đến",
+        artist: "Lê Bảo Bình",
+        img: tbnail8,
+    },
+    {
+        src: CoPhuongTuThuong,
+        title: "Cô phương tự thưởng",
+        artist: "Lời việt : Doãn Hiếu",
+        img: tbnail9,
+    },
+    {
+        src: TrucXinh,
+        title: "Trúc xinh",
+        artist: "Minh Vương M4U - Việt ACV",
+        img: tbnail10,
+    },
+]
