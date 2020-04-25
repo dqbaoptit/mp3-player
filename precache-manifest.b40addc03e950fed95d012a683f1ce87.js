e1906ef90c59b5d6e4dab6405a2463b961947d35
Bao <dqbaoptit@gmail.com>
2020-04-25 11:46:44 +0700

    Updates

---
+++ precache-manifest.b40addc03e950fed95d012a683f1ce87.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89b637018f11e0b9b0be5207fe20add3",
+    "url": "/mp3-player/index.html"
+  },
+  {
+    "revision": "e1d2111b9333044fb725",
+    "url": "/mp3-player/static/css/main.e8150f8f.chunk.css"
+  },
+  {
+    "revision": "dd97ed2c75996e8ce6be",
+    "url": "/mp3-player/static/js/2.ee7c5ac6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/mp3-player/static/js/2.ee7c5ac6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1d2111b9333044fb725",
+    "url": "/mp3-player/static/js/main.7e65b322.chunk.js"
+  },
+  {
+    "revision": "6485c384d9f474ca428b",
+    "url": "/mp3-player/static/js/runtime-main.0469ea87.js"
+  },
+  {
+    "revision": "b515bd18fcb21d5dabd576ddf74dfeaa",
+    "url": "/mp3-player/static/media/Ai-Doi-Minh-Duoc-Mai-Thanh-Hung.b515bd18.mp3"
+  },
+  {
+    "revision": "2f0803774eb69e4ba818d504d72552f1",
+    "url": "/mp3-player/static/media/Chi-Khi-Anh-Mot-Minh-Vu-Duy-Khanh-Dat-G.2f080377.mp3"
+  },
+  {
+    "revision": "8efb7dc6c619b22ddbcd0d74d501ff2c",
+    "url": "/mp3-player/static/media/Dung-Lo-Anh-Doi-Ma-Mr-Siro.8efb7dc6.mp3"
+  },
+  {
+    "revision": "38854b5f32aef423a7a2a29458c6b8db",
+    "url": "/mp3-player/static/media/Em-Co-Biet-H2K.38854b5f.mp3"
+  },
+  {
+    "revision": "ef51045ecc13616db46ac6db6e014179",
+    "url": "/mp3-player/static/media/Hon-Ca-Yeu-Duc-Phuc.ef51045e.mp3"
+  },
+  {
+    "revision": "932e97bb82dc3b4c783726dd373901a9",
+    "url": "/mp3-player/static/media/La-Duyen-Khong-Phan-Chung-Thanh-Duy.932e97bb.mp3"
+  },
+  {
+    "revision": "fbafbb7331f22f39fc4e64a191ee55a6",
+    "url": "/mp3-player/static/media/So-Phai-Ket-Thuc-Nhat-Phong.fbafbb73.mp3"
+  },
+  {
+    "revision": "f58b3a57beadea86d7c8074d6d61fece",
+    "url": "/mp3-player/static/media/Thich-Thi-Den-Le-Bao-Binh.f58b3a57.mp3"
+  },
+  {
+    "revision": "108c2ee23c90acdccfaaacb129cefd8e",
+    "url": "/mp3-player/static/media/Tinh-Sau-Thien-Thu-Muon-Loi-Doan-Hieu.108c2ee2.mp3"
+  },
+  {
+    "revision": "6e29fc5d522d35d2c7f5e35379741c89",
+    "url": "/mp3-player/static/media/Truc-Xinh-Minh-Vuong-M4U-Viet-ACV.6e29fc5d.mp3"
+  },
+  {
+    "revision": "bf071fb1a2561e9bc8d5dc1c71300e95",
+    "url": "/mp3-player/static/media/aidoiminh.bf071fb1.jpg"
+  },
+  {
+    "revision": "48e8b851ddb3548418641d7142f885db",
+    "url": "/mp3-player/static/media/chungthanhduy.48e8b851.jpg"
+  },
+  {
+    "revision": "00afc60ae088fad35cfe4c760538ed60",
+    "url": "/mp3-player/static/media/ckamm.00afc60a.jpg"
+  },
+  {
+    "revision": "2c6266165f27abc2f2466f3b49fbc938",
+    "url": "/mp3-player/static/media/cptt.2c626616.jpg"
+  },
+  {
+    "revision": "7fd47b1f83153534b5f0f5fec952a804",
+    "url": "/mp3-player/static/media/dunglo.7fd47b1f.jpg"
+  },
+  {
+    "revision": "bdbc969bcc438239c4e6f4ff11241ce4",
+    "url": "/mp3-player/static/media/emcobiet.bdbc969b.jpg"
+  },
+  {
+    "revision": "6e7596a0a0cddd5658380a915a3e5d20",
+    "url": "/mp3-player/static/media/honcayeu.6e7596a0.jpg"
+  },
+  {
+    "revision": "0ba0c3fc5100b1be1ad2ee900a378e96",
+    "url": "/mp3-player/static/media/spketthuc.0ba0c3fc.jpg"
+  },
+  {
+    "revision": "7378f9d5f9b7e5ae92729306a79c71d7",
+    "url": "/mp3-player/static/media/thithiden.7378f9d5.jpg"
+  },
+  {
+    "revision": "9a5a80990cccd2838445e232d46e5b4e",
+    "url": "/mp3-player/static/media/trucxinh.9a5a8099.jpg"
+  }
+]);
